da0a0389533c7883378e6086cafff97047e4b5d9
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-11 20:47:49 +0530

    list of users display done

--- src/components/AuthLoading.js
+++ src/components/AuthLoading.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {
-  ActivityIndicator,
-  AsyncStorage,
-  StatusBar,
-  View,
+    ActivityIndicator,
+    AsyncStorage,
+    StatusBar,
+    View,
 } from 'react-native';
 
 import User from '../User';
@@ -11,42 +11,42 @@ import User from '../User';
 import firebase from 'firebase';
 
 export default class AuthLoading extends React.Component {
-  constructor(props) {
-    super(props);
-    this._bootstrapAsync();
-  }
+    constructor(props) {
+        super(props);
+        this._bootstrapAsync();
+    }
 
-  componentWillMount(){
-       // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyCd64cQfVGZgBunig_FJtn0pwOcBD6dVGg",
-    authDomain: "fir-chat-c7b72.firebaseapp.com",
-    databaseURL: "https://fir-chat-c7b72.firebaseio.com",
-    projectId: "fir-chat-c7b72",
-    storageBucket: "",
-    messagingSenderId: "1033817601932",
-    appId: "1:1033817601932:web:0de4b87fbca5c84d"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  }
+    componentDidMount() {
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+            apiKey: "AIzaSyCd64cQfVGZgBunig_FJtn0pwOcBD6dVGg",
+            authDomain: "fir-chat-c7b72.firebaseapp.com",
+            databaseURL: "https://fir-chat-c7b72.firebaseio.com",
+            projectId: "fir-chat-c7b72",
+            storageBucket: "",
+            messagingSenderId: "1033817601932",
+            appId: "1:1033817601932:web:0de4b87fbca5c84d"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+    }
 
-  // Fetch the token from storage then navigate to our appropriate place
-  _bootstrapAsync = async () => {
-    User.phone = await AsyncStorage.getItem('userPhone');
+    // Fetch the token from storage then navigate to our appropriate place
+    _bootstrapAsync = async () => {
+        User.phone = await AsyncStorage.getItem('userPhone');
 
-    // This will switch to the App screen or Auth screen and this loading
-    // screen will be unmounted and thrown away.
-    this.props.navigation.navigate(User.phone ? 'App' : 'Auth');
-  };
+        // This will switch to the App screen or Auth screen and this loading
+        // screen will be unmounted and thrown away.
+        this.props.navigation.navigate(User.phone ? 'App' : 'Auth');
+    };
 
-  // Render any loading content that you like here
-  render() {
-    return (
-      <View>
-        <ActivityIndicator />
-        <StatusBar barStyle="default" />
-      </View>
-    );
-  }
+    // Render any loading content that you like here
+    render() {
+        return (
+            <View>
+                <ActivityIndicator />
+                <StatusBar barStyle="default" />
+            </View>
+        );
+    }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,62 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { SafeAreaView, View, Text, TouchableOpacity, FlatList } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
+import firebase from 'firebase';
 
 import User from '../User';
 import styles from '../../constants/styles';
 
 export default class Home extends React.Component {
   static navigationOptions = {
-    title: 'Chats'
+    title: 'Chats',
+    headerTitleStyle: {
+      textAlign: 'center',
+      flexGrow: 1,
+      alignSelf: 'center',
+      fontWeight: 'bold',
+    }
   }
 
-  _logOut =  async() => {
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    let dbRef = firebase.database().ref('users');
+    dbRef.on('child_added', (val) => {
+      let person = val.val();
+      person.phone = val.key;
+      this.setState((prevState) => {
+        return {
+          users: [...prevState.users, person]
+        }
+      })
+    });
+  }
+
+  _logOut = async () => {
     await AsyncStorage.clear();
     this.props.navigation.navigate('Auth');
   }
+
+  renderRow = ({ item }) => {
+    return (
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate('Chat', item)}
+        style={{ padding: 10, borderBottomColor: '#ccc', borderBottomWidth: 1 }}>
+        <Text style={{ fontSize: 20 }}>{item.name}</Text>
+      </TouchableOpacity>
+    )
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>
-          {User.phone}
-        </Text>
-        <TouchableOpacity onPress={this._logOut}>
-            <Text>Logout</Text>
-        </TouchableOpacity>
-      </View>
+      <SafeAreaView>
+        <FlatList
+          data={this.state.users}
+          renderItem={this.renderRow}
+          keyExtractor={(item) => item.phone}
+        />
+      </SafeAreaView>
     );
   }
 }
