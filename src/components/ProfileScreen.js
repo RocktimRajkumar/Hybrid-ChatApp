97bdfb3af2e0b3db4f5c095bbd7e42b96f34fc57
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-12 23:54:04 +0530

    src > components > ProfileScreen : ProfileScreen.js created

---
+++ src/components/ProfileScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class ProfileScreen extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Profile Page
+                </Text>
+            </View>
+        )
+    }
+}
