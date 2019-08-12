c81d77e88bd07a6a518439d1a03fbca29b5f804f
Rocktim <rajkumar.rocktim@gmail.com>
2019-08-13 00:12:02 +0530

    src > components > ProfileScreen : username change done

--- src/components/ProfileScreen.js
+++ src/components/ProfileScreen.js
@@ -1,14 +1,52 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { SafeAreaView, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
+import User from '../User';
+import styles from '../../constants/styles';
+import firebase from 'firebase';
 
 export default class ProfileScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Profile',
+        headerTitleStyle: {
+            textAlign: 'center',
+            flexGrow: 1,
+            alignSelf: 'center',
+            fontWeight: 'bold',
+        },
+    }
+
+    state = {
+        name: User.name
+    }
+
+    changeName = async () => {
+        if (this.state.name < 3) {
+            Alert.alert('Error', 'Please enter a valid name');
+        } else if (User.name !== this.state.name) {
+            firebase.database().ref('users').child(User.phone).set({ name: this.state.name });
+            Alert.alert('Success', 'Name changed successful');
+        }
+    }
+
     render() {
         return (
-            <View>
-                <Text>
-                    Profile Page
+            <SafeAreaView style={styles.container}>
+                <Text style={{ fontSize: 20 }}>
+                    {User.phone}
                 </Text>
-            </View>
+                <TextInput
+                    style={styles.input}
+                    value={this.state.name}
+                    onChangeText={(text) => {
+                        this.setState({
+                            name: text
+                        })
+                    }}
+                />
+                <TouchableOpacity onPress={this.changeName}>
+                    <Text style={styles.buttonText}>Change Name</Text>
+                </TouchableOpacity>
+            </SafeAreaView>
         )
     }
 }
